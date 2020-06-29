7425331dd4d5175c4c99c664480b4912739d87e5
Gaurav Nemade <gnemade51@gmail.com>
2020-06-29 19:45:10 +0530

    Started with work code

--- Working_Folder/Updating_rpi_code.py
+++ Working_Folder/Updating_rpi_code.py
@@ -8,9 +8,10 @@ img = cv2.imread("book_page-1309x1536.jpg")
 img = cv2.resize(img, None, fx=0.5, fy=0.5)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-adaptive_threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 55, 11)
+adaptive_threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 85, 11)
 
-text = pytesseract.image_to_string(adaptive_threshold)
+config = "--psm 3"
+text = pytesseract.image_to_string(adaptive_threshold, config = config)
 print(text)
 
 # cv2.imshow('Img', img)
